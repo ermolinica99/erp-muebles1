52f96b21b94908fa8e2e23c936584010333653cc
ermolinica99 <ermolinica99@gmail.com>
2026-01-04 00:01:00 +0100

    Create urls.py

---
+++ backend/clientes/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import ClienteViewSet
+
+router = DefaultRouter()
+router.register(r'clientes', ClienteViewSet)
+
+urlpatterns = router.urls
